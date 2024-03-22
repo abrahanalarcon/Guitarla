9700b0f0593b0a8028e88a24f9a04d61bec1b6ee
abrahanalarcon <moiseshazim@gmail.com>
2024-03-22 19:30:06 -0400

    agregando a que el precio total no se visualice si el carrito esta vacio

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,7 +24,7 @@ function Header ({cart})  {
                            <p className="text-center">El carrito esta vacio</p>
                         ) : (
                         
-                    
+                         <>
                          <table className="w-100 table">
                             <thead>
                                 <tr>
@@ -77,10 +77,13 @@ function Header ({cart})  {
                             </tbody>
                          </table>
 
-                        )}
+                        
 
                         <p className="text-end">Total pagar: <span className="fw-bold">$899</span></p>
                         <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
+
+                        </>
+                        )}
                     </div>
                 </div>
             </nav>
