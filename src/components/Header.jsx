1638a48a925ab43f847fe54519a8a325108087b6
abrahanalarcon <moiseshazim@gmail.com>
2024-03-22 20:18:44 -0400

    calculando el total a pagar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ function Header ({cart})  {
   
   //State Derivado 
   const isEmpty = () => cart.length === 0
+  const cartTotal = () => cart.reduce( (total, item ) => total + (item.quantity * item.price), 0 )
 
   return (
     <header className="py-5 header">
@@ -79,7 +80,7 @@ function Header ({cart})  {
 
                         
 
-                        <p className="text-end">Total pagar: <span className="fw-bold">$899</span></p>
+                        <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal()}</span></p>
                         <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
 
                         </>
